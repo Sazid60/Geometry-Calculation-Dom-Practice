f46df0da7dbe3e6d6f59c7fd69e548aaf814fba6
Sazid60 <shahnawazsazid60@gmail.com>
2024-02-13 08:15:53 +0600

    Triangle Calculation Complete

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,43 @@
+
+
+
+
+// console.log('connected')
+
+// get bse and height of the triangle and calculate the area using the provided formula & display the output 
+// step-1 : get base Value of te triangle
+// step-2: added an id in the base input field
+// step-3:used getElementById to access the input field
+// step-4: get value from the input filed (value is string now)
+// step-5: convert the value in number using parseFloat()
+
+
+function calculateTriangleArea() {
+    // console.log('inside Function')
+    ///////////////////////////////////////////////// Triangle Base Value 
+
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    // console.log(triangleBaseText);
+    // console.log(typeof triangleBaseText); // string value dekhabe even number dileo
+    const base = parseFloat(triangleBaseText);
+    // console.log(typeof base)
+    console.log(base)
+
+    ///////////////////////////////////////////////// Triangle Height Value 
+    const triangleHeightInput = document.getElementById('triangle-height');
+    // console.log(triangleHeightInput);
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText)
+    console.log(height);
+
+    // Calculate Triangle Area
+    const area = 0.5 * base * height 
+    console.log('Area of the Triangle is:', area)
+
+    //Display Area 
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+}
+
+
