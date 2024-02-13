4e699580e275732144081f49a095f90b0722ee01
Sazid60 <shahnawazsazid60@gmail.com>
2024-02-13 09:13:41 +0600

    Rectangle Added

--- scripts/rectangel-calculate.js
+++ scripts/rectangel-calculate.js
@@ -1,3 +1,22 @@
 function calculateRectangleArea(){
-    console.log('rectangle')
+    // console.log('rectangle')
+    //Get Width 
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(typeof width);
+
+    //get Length
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    // console.log(lengthText)
+    const length =  parseFloat(lengthText);
+    console.log(typeof length);
+
+    //calculate Rectangle Area 
+    const area = width * length;
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
 }
